0cc9ff6265d115a44a62a76c53227cb34cd05898
Ksheetiz <ksheetiz.agrahari@oniondev.com>
2025-04-29 14:06:46 +0530

    single mws report fix

--- src/components/kyl_rightSidebar.jsx
+++ src/components/kyl_rightSidebar.jsx
@@ -31,7 +31,7 @@ const KYLRightSidebar = ({
     currentLayer,
     setCurrentLayer,
     mapRef,
-    onAnalyzeClick,
+    //onAnalyzeClick,
     onResetMWS,
     selectedMWSProfile,
 }) => {
@@ -219,16 +219,16 @@ const KYLRightSidebar = ({
                             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                             </svg>
-                            Download Report
+                            View MWS's Report
                         </button>
-                        <button 
+                        {/* <button 
                         onClick={onAnalyzeClick}
                         className="w-full flex items-center justify-center gap-2 text-indigo-600 py-2 text-sm hover:bg-indigo-50 rounded-md">
                             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                             </svg>
                             Analyze micro-watershed profiles
-                        </button>
+                        </button> */}
                     </div>
                 )}
             </div>
