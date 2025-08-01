6440b59d3af4f45e55fb2f8515809920ac721265
Ksheetiz Agrahari <ksheetiz.agrahari@oniondev.com>
2025-08-01 12:05:08 +0530

    Auto stash before rebase of "revamp_landing_page" onto "development"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LE_page from "./pages/LE_page";
-import LandingPage from "./pages/landingPage";
 import KYLDashboardPage from "./pages/kyl_dashboard";
 import LandscapeExplorer from "./pages/LandscapeExplorer";
 import LEHomepage from "./pages/LE_homepage";
@@ -10,10 +8,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<LandingPage/>}/> */}
           <Route path="/" element={<LEHomepage />} />
           <Route path="/kyl_dashboard" element={<KYLDashboardPage />} />
-          {/* <Route path="/landscape_explorer" element={<LE_page />} /> */}
         <Route path="/download_layers" element={<LandscapeExplorer/>}/>
         </Routes>
       </BrowserRouter>
