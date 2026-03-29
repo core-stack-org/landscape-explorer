eed1d9c3c54fceb84b75cf3b17e2758cd7cde4c8
Chandana Sree <your-email@example.com>
2026-03-29 20:28:10 +0530

    Updated homescreen

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-white shadow-2xl">
       <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex justify-center items-center">
         <p className="text-center text-sm sm:text-base md:text-lg font-semibold text-gray-800 leading-relaxed">
-          <span className="font-bold">CoRE Stack</span>: A commoning
+          <span className="font-bold">CoStack</span>: A commoning
           approach — Tech stacks for communities; 2025
         </p>
       </div>
