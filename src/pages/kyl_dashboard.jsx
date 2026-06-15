78a3f3261dfc90ef46534ce82f52b1cae4d0f20f
Ksheetiz Agrahari <ksheetiz.agrahari@oniondev.com>
2026-06-15 12:47:16 +0530

    Chore : Added Landuse Filters

--- src/pages/kyl_dashboard.jsx
+++ src/pages/kyl_dashboard.jsx
@@ -1972,17 +1972,8 @@ const KYLDashboardPage = () => {
             const tempLayer = await getImageLayer(
               `LULC_level_3`,
               `LULC_${lulcYear}_${transformName(district.label)}_${transformName(block.label)}_level_3`,
-              true
-            );
-            mapRef.current.addLayer(tempLayer);
-            tempArr[i].layerRef[0] = tempLayer;
-          }
-          if (tempArr[i].name === "built_up_area") {
-            mapRef.current.removeLayer(tempArr[i].layerRef[0]);
-            const tempLayer = await getImageLayer(
-              `LULC_level_1`,
-              `LULC_${lulcYear}_${transformName(district.label)}_${transformName(block.label)}_level_1`,
-              true
+              true,
+              "lulc_land_use_KYL"
             );
             mapRef.current.addLayer(tempLayer);
             tempArr[i].layerRef[0] = tempLayer;
